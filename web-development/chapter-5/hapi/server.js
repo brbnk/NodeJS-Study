736a5bd31467114c56fd2db9e94ab186d8ea4003
Bruno Nakayabu <bruno.nakayabu@gmail.com>
2019-09-13 14:15:15 +0000

    Server-Side frameworks overall

---
+++ web-development/chapter-5/hapi/server.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const Hapi = require("@hapi/hapi")
+
+const init = async () => { 
+    
+    const server = Hapi.server({
+        host: "localhost",
+        port: 3000
+    })
+
+    server.route({
+        method: "GET",
+        path: "/",
+        handler: (request, h) => { 
+            return "Hello, World!"
+        }
+    })
+
+    await server.start()
+    console.log("Server running on %s", server.info.uri)
+}
+
+process.on("unhadledRejection", (err) => { 
+    console.log(err)
+    process.exit(1)
+})
+
+init();
