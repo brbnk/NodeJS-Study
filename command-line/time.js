390d0723d3a44c4d19c866a982ab8788ccf24739
Bruno Nakayabu <bruno.nakayabu@gmail.com>
2019-09-25 14:35:00 -0300

    command-line

---
+++ command-line/time.js
@@ -0,0 +1,7 @@
+process.stdin.pipe(process.stdout)
+
+const start = Date.now()
+process.on("exit", () => {
+    const timeTaken = Date.now() - start
+    console.error(`Time (s): ${ timeTaken / 1000 }`)
+})
